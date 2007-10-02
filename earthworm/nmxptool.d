b01d39c79826d91397b57bffeed21424b1e6f1fd
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2007-10-02 08:33:43 +0000

    Update for nmxptool
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@406 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- earthworm/nmxptool.d
+++ earthworm/nmxptool.d
@@ -1,5 +1,5 @@
 #
-#                     Configuration File for nmxp2ew
+#                     Configuration File for nmxptool
 #
 MyModuleId       MOD_NMXPTOOL
 RingName         WAVE_RING       # Transport ring to write data to.
@@ -9,8 +9,8 @@ LogFile               2          # 1 -> Keep log, 0 -> no log file
                                  # 2 -> write to module log but not stderr/stdout
 Verbosity      0		 # Set level of verbosity.
 
-nmxphost   naqs1a.int.ingv.it    # Host address of the SeedLink server
-nmxpport       28000             # Port number of the SeedLink server
+nmxphost   naqs1a.int.ingv.it    # Host address of the NaqsServer server
+nmxpport       28000             # Port number of the NaqsServer server
 
 #ForceTraceBuf1 0                # On systems that support TRACEBUF2
                                  # messages this flag will force the module
