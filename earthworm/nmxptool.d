9d4247a57518616d8be4903ff0e98ae43db1816b
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2007-10-09 07:43:11 +0000

    Update
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@440 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- earthworm/nmxptool.d
+++ earthworm/nmxptool.d
@@ -18,10 +18,11 @@ NmxpPortDAP          28002                 # Port number of the DataServer serve
                                            # to create TRACEBUF messages instead.
                                            # Most people will never need this.
 
-TimeoutRecv          0                     # Time-out in seconds for flushing queued data for each channel
+#TimeoutRecv          30                    # Time-out in seconds for flushing queued data for each channel.
                                            # Useful for Data On Demand (i.e. channel HL) (Default 0. No time-out)
 
-MaxTolerableLatency  120                   # Max tolerable latency for each channel (In general not use with TimeoutRecv)
+MaxTolerableLatency  120                   # Max tolerable latency for each channel. (Default 600 sec.)
+                                           # In general, it is not use with parameter TimeoutRecv.
 
 DefaultNetworkCode   IV                    # Default network code where in Channel is not declared
 
