7fa9d4cd51f943af44f43b93276a40ea82a4fd63
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2008-02-14 07:33:53 +0000

    Added option for gap tollerance
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@696 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- scripts/nmxp_sds_check.sh
+++ scripts/nmxp_sds_check.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Syntax: cmd <nmxptool_channelfile> <year> <jday>
+# Syntax: cmd <nmxptool_channelfile> <year> <jday> [<gap_tol>]
+#         gap_tol is option -G of qmerge
 # 
 # TODO: add location
 #
@@ -25,6 +26,11 @@ fi
 NMXPCHANNELFILE=$1
 YEAR=$2
 JDAY=`echo $3 | sed -e "s/^[0]*//"`
+
+if [ ! -z $4 ]; then
+	QMERGEOPT="-G $4"
+	echo "qmerge option: ${QMERGEOPT}"
+fi
 
 # variables dependent on the input parameters
 
@@ -51,14 +57,13 @@ DIRARCHIVESDS=/home/sysop/seiscomp/acquisition/archive
 DIRLOG=$(dirname $0)/log
 NOW=$(date "+%Y%m%dx%H%M%S")
 QMERGE=qmerge
+FILEOUTARCH=qmerge.out.txt
 
 while read LINE
 do
 	NET=`echo $LINE | cut -d'.' -f 1`
 	STA=`echo $LINE | cut -d'.' -f 2`
 	CHANWILD=`echo $LINE | cut -d'.' -f 3`
-
-	echo "$NET $STA $CHANWILD"
 
 	if [ "$CHANWILD" == "HH?" ]; then
 		CHANNELCOMPS="HHZ HHN HHE"
@@ -74,7 +79,15 @@ do
 		FILEARCH=${DIRARCHIVESDS}/${YEAR}/${NET}/${STA}/${CHAN}.D/${NET}.${STA}.${LOC}.${CHAN}.D.${YEAR}.${JDAY}
 
 		if [ -f ${FILEARCH} ]; then
-			$QMERGE -n $FILEARCH
+			${QMERGE} ${QMERGEOPT} -n ${FILEARCH} > ${FILEOUTARCH}
+			NSEGMENTS=`grep ${YEAR}.${JDAY} ${FILEOUTARCH} | wc -l`
+			NGAP=$(( ${NSEGMENTS} - 1 ))
+
+			echo ""
+			echo "${NET}.${STA}.${CHAN} ${YEAR}.${JDAY} ${QMERGEOPT}: ${NGAP} gaps "
+
+			cat ${FILEOUTARCH}
+
 		else
 			echo "WARNING: ${FILEARCH} does not exists!"
 		fi
