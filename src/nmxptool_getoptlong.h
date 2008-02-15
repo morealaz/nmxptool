9403e91d8d7f4c4faa07cbcd55c96c546677035f
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2008-02-15 08:03:58 +0000

    Incremented PACKAGE_BUILD
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@704 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- src/nmxptool_getoptlong.h
+++ src/nmxptool_getoptlong.h
@@ -7,7 +7,7 @@
  * 	Istituto Nazionale di Geofisica e Vulcanologia - Italy
  *	quintiliani@ingv.it
  *
- * $Id: nmxptool_getoptlong.h,v 1.63 2008-02-12 13:11:12 mtheo Exp $
+ * $Id: nmxptool_getoptlong.h,v 1.64 2008-02-15 08:03:58 mtheo Exp $
  *
  */
 
@@ -18,7 +18,7 @@
 #include <stdint.h>
 
 /* For stable release set to "" */
-#define PACKAGE_BUILD "-beta19"
+#define PACKAGE_BUILD "-beta20"
 
 #define DEFAULT_NETWORK  	"XX"
 #define DEFAULT_PORT_DAP 	28002
