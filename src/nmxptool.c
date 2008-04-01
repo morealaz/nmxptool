ea4591af6cc73f59c32925922b0913e6738657a8
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2008-04-01 14:12:03 +0000

    Minor change
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@1016 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- src/nmxptool.c
+++ src/nmxptool.c
@@ -7,7 +7,7 @@
  * 	Istituto Nazionale di Geofisica e Vulcanologia - Italy
  *	quintiliani@ingv.it
  *
- * $Id: nmxptool.c,v 1.170 2008-04-01 14:10:15 mtheo Exp $
+ * $Id: nmxptool.c,v 1.171 2008-04-01 14:12:03 mtheo Exp $
  *
  */
 
@@ -1483,9 +1483,9 @@ static void *p_nmxp_sendAddTimeSeriesChannel(void *arg) {
     while(times_channel > 0) {
 	nmxp_sendAddTimeSeriesChannel(naqssock, channelList_subset, params.stc, params.rate,
 		(params.flag_buffered)? NMXP_BUFFER_YES : NMXP_BUFFER_NO, params.n_channel, params.usec, (i==0)? 1 : 0);
-	nmxp_usleep(params.usec+1);
 	times_channel--;
 	i++;
+	nmxp_usleep(params.usec+1);
     }
     nmxp_log(NMXP_LOG_WARN, NMXP_LOG_D_ANY, "End requests of channels!\n");
 
