29d5be98b5fc8c3d22c225ab4dd4c45752b8f0c9
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2008-06-17 12:07:54 +0000

    Start
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@1084 3cd66e75-5955-46cb-a940-c26e5fc5497d

---
+++ no_dist/scripts/nmxp_cvs2cl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIRNAME=`dirname $0`
+
+FROM_TAG=nmxptool_1_2_0
+TO_TAG=nmxptool_1_2_1
+
+UFILE=${DIRNAME}/UFILE
+HEADERFILE=${DIRNAME}/HEADERFILE
+
+date "+%Y-%m-%d %H:%M" > ${HEADERFILE}
+echo "" >> ${HEADERFILE}
+echo "	* nmxptool:" >> ${HEADERFILE}
+echo "" >> ${HEADERFILE}
+echo "	  Open-Source and Cross-Platform software for Nanometrics seismic data acquisition" >> ${HEADERFILE}
+echo "	  Matteo Quintiliani - Istituto Nazionale di Geofisica e Vulcanologia - Italy" >> ${HEADERFILE}
+echo "	  Mail bug reports and suggestions to <quintiliani@ingv.it> " >> ${HEADERFILE}
+echo "" >> ${HEADERFILE}
+echo "	  ChangeLog from tag ${FROM_TAG} to ${TO_TAG}" >> ${HEADERFILE}
+echo "" >> ${HEADERFILE}
+
+cat ${HEADERFILE} | cvs2cl.pl -r --delta ${FROM_TAG}:${TO_TAG} -U ${UFILE} -S --utc -I "^no_dist/*" --header -
+
