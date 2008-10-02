2e1a207321fa8ee3ef5e6ae76b6f4598f6baa129
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2008-10-02 10:25:44 +0000

    Added improvemnts
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@1146 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- scripts/nmxp_sds_mgr.sh
+++ scripts/nmxp_sds_mgr.sh
@@ -11,19 +11,24 @@
 
 . `dirname $0`/nmxp_sds.conf
 
-STATION_LIST=`cat nmxp_sds_station_list.conf`
-# ./nmxptool_filelist.sh naqs2a.int.ingv.it data "\.HL[NEZ]"
+NOW=$(date "+%Y%m%dx%H%M%S")
+FILELOG=`dirname $0`/nmxp_sds_mgr_${NOW}.log
+
+FILE_STATION_LIST=`dirname $0`/nmxp_sds_station_list.conf
+`dirname $0`/nmxp_stationlist.sh naqs2a.int.ingv.it data "\.HL[NEZ]" | sed -e "s/^/${DEFAULTNET}\./" > ${FILE_STATION_LIST} 2> ${FILELOG}
+STATION_LIST=`cat ${FILE_STATION_LIST}`
 
 NMXP_SDS=`dirname $0`/nmxp_sds.sh 
 
 JDAY=${JDAYSTART}
 while [ $JDAY -le ${JDAYEND} ]; do
-	echo "Download for year/jday ${YEAR}.${JDAY}"
+	echo "Download for year/jday ${YEAR}.${JDAY}" >> ${FILELOG} 2>&1
 	for STATION in ${STATION_LIST}; do
 		NET=`echo ${STATION} | cut -f 1 -d'.'`
 		STA=`echo ${STATION} | cut -f 2 -d'.'`
 		CHAN=`echo ${STATION} | cut -f 3 -d'.'`
-		echo ${NMXP_SDS} ${HOSTNAME} ${YEAR} ${NET} ${STA} ${CHAN} ${JDAY} ${OVERRIDE}
+		${NMXP_SDS} ${HOSTNAME} ${YEAR} ${NET} ${STA} ${CHAN} ${JDAY} ${OVERRIDE} >> ${FILELOG} 2>&1
+
 	done
 	JDAY=$(($JDAY + 1))
 done
