9de5c65ca72e4be45fc030c701457c3dc342aa84
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2009-10-06 14:41:14 +0000

    Minor change
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@1299 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- no_dist/scripts/nmxp_sp_update_channel_acquisition.sh
+++ no_dist/scripts/nmxp_sp_update_channel_acquisition.sh
@@ -12,6 +12,6 @@ NAQSERVERS="naqs1a.int.ingv.it naqs2a.int.ingv.it naqs1b.int.ingv.it naqs2b.int.
 NAQSPORT=28000
 
 for NAQSSERVER in ${NAQSERVERS}; do
-    ${NMXPTOOLBIN} -H ${NAQSSERVER} -P ${NAQSPORT} -L | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/sp_update_channel_acquisition('\1', '\2', 0, '${NAQSSERVER}', ${NAQSPORT});/"
+    ${NMXPTOOLBIN} -H ${NAQSSERVER} -P ${NAQSPORT} -L | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/CALL sp_update_channel_acquisition('\1', '\2', 0, '${NAQSSERVER}', ${NAQSPORT});/"
 done
 
