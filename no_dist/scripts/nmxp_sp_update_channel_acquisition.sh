f5cf2105156ccdd6854730cbebb6ee82b4d964ba
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2009-10-06 14:57:46 +0000

    Added slinktool support
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@1300 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- no_dist/scripts/nmxp_sp_update_channel_acquisition.sh
+++ no_dist/scripts/nmxp_sp_update_channel_acquisition.sh
@@ -7,11 +7,19 @@
 ############################################################################
 
 NMXPTOOLBIN=../../src/nmxptool
+SLINKTOOLBIN=/Users/mtheo/Desktop/soft/unix_sources/src/slinktool/slinktool
 
 NAQSERVERS="naqs1a.int.ingv.it naqs2a.int.ingv.it naqs1b.int.ingv.it naqs2b.int.ingv.it"
 NAQSPORT=28000
 
+SLSERVERS="hsl2.int.ingv.it"
+SLPORT=18000
+
 for NAQSSERVER in ${NAQSERVERS}; do
-    ${NMXPTOOLBIN} -H ${NAQSSERVER} -P ${NAQSPORT} -L | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/CALL sp_update_channel_acquisition('\1', '\2', 0, '${NAQSSERVER}', ${NAQSPORT});/"
+    ${NMXPTOOLBIN} -H ${NAQSSERVER} -P ${NAQSPORT} -L | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/CALL sp_update_channel_acquisition('', '\1', '\2', 0, '${NAQSSERVER}', ${NAQSPORT});/"
 done
 
+
+for SLSERVER in ${SLSERVERS}; do
+    ${SLINKTOOLBIN} -Q ${SLSERVER}:${SLPORT} | grep " D " | sed -e "s/[ ][ ]*/ /g" | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\) \([^ ][^ ]*\) \([^ ]\) .*$/CALL sp_update_channel_acquisition('\1', '\2', '\3', 0, '${SLSERVER}', ${SLPORT});/g"
+done
