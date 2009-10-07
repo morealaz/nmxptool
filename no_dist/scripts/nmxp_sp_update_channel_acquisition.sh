ffe89b7e8ba33fc3e96d0e99bf26bdffeb5704fb
Matteo Quintiliani <matteo.quintiliani@ingv.it>
2009-10-07 12:42:53 +0000

    Added DATASERVERS and fixed some other parameters
    git-svn-id: file:///home/quintiliani/svncopy/nmxptool/trunk@1301 3cd66e75-5955-46cb-a940-c26e5fc5497d

--- no_dist/scripts/nmxp_sp_update_channel_acquisition.sh
+++ no_dist/scripts/nmxp_sp_update_channel_acquisition.sh
@@ -9,17 +9,23 @@
 NMXPTOOLBIN=../../src/nmxptool
 SLINKTOOLBIN=/Users/mtheo/Desktop/soft/unix_sources/src/slinktool/slinktool
 
+DATASERVERS="naqs1a.int.ingv.it naqs2a.int.ingv.it naqs1b.int.ingv.it naqs2b.int.ingv.it"
 NAQSERVERS="naqs1a.int.ingv.it naqs2a.int.ingv.it naqs1b.int.ingv.it naqs2b.int.ingv.it"
 NAQSPORT=28000
+DATAPORT=28002
 
 SLSERVERS="hsl2.int.ingv.it"
 SLPORT=18000
 
 for NAQSSERVER in ${NAQSERVERS}; do
-    ${NMXPTOOLBIN} -H ${NAQSSERVER} -P ${NAQSPORT} -L | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/CALL sp_update_channel_acquisition('', '\1', '\2', 0, '${NAQSSERVER}', ${NAQSPORT});/"
+    ${NMXPTOOLBIN} -H ${NAQSSERVER} -P ${NAQSPORT} -L | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/CALL sp_update_channel_acquisition('', '\1', '\2', '', 0, '${NAQSSERVER}', ${NAQSPORT}, 'NaqsServer');/"
+done
+
+for DATASERVER in ${DATASERVERS}; do
+    ${NMXPTOOLBIN} -H ${DATASERVER} -P ${DATAPORT} -l | sed -e "s/[ ][ ]*/ /g" | cut -f 3 -d' ' | tr '.' ' ' | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\)/CALL sp_update_channel_acquisition('', '\1', '\2', '', 0, '${DATASERVER}', ${DATAPORT}, 'DataServer');/"
 done
 
 
 for SLSERVER in ${SLSERVERS}; do
-    ${SLINKTOOLBIN} -Q ${SLSERVER}:${SLPORT} | grep " D " | sed -e "s/[ ][ ]*/ /g" | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\) \([^ ][^ ]*\) \([^ ]\) .*$/CALL sp_update_channel_acquisition('\1', '\2', '\3', 0, '${SLSERVER}', ${SLPORT});/g"
+    ${SLINKTOOLBIN} -Q ${SLSERVER}:${SLPORT} | grep " D " | sed -e "s/[ ][ ]*/ /g" | sed -e "s/\([^ ][^ ]*\) \([^ ][^ ]*\) \([^ ][^ ]*\) \([^ ]\) .*$/CALL sp_update_channel_acquisition('\1', '\2', '\3', '', 0, '${SLSERVER}', ${SLPORT}, 'SeedLink');/g"
 done
